5d4e491c8b418cfe7503aed593b21cc5b0eb2bf9
pengwin <ikruchkov0@gmail.com>
2022-08-27 01:03:28 +0200

    Add localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import type { Component } from 'solid-js';
+import {
+    I18nContext,
+    createI18nContext,
+} from '@solid-primitives/i18n';
+
 import Comp from './Comp';
+
+const dict = {
+    ru: {
+      'Comp': {
+        'SwitchLang': 'Переключить язык',
+        'Flip': 'Перевернуть'
+      }
+    },
+    en: {
+      'Comp': {
+        'SwitchLang': 'Switch lang',
+        'Flip': 'Flip'
+      }
+    },
+};
+
+const value = createI18nContext(dict, 'en');
 
 const App: Component = () => {
     return (
-        <div class="container mx-auto">
-            <h1>Hello world!!!!</h1>
-            <Comp text="test" />
-        </div>
+        <I18nContext.Provider value={value}>
+            <div class="container mx-auto">
+                <h1>Hello world!!!!</h1>
+                <Comp text="test" />
+            </div>
+        </I18nContext.Provider>
     );
 };
 
