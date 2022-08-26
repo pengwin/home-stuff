63ea8ff0313f4423135f68a06e88a0012d1bec53
Ivan Kruchkov <ikruchkov0@gmail.com>
2022-08-27 01:11:49 +0200

    Add localization (#28)
    * Add localization
    
    * Fix format and lint issues

--- src/Comp.test.tsx
+++ src/Comp.test.tsx
@@ -1,39 +1,123 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, Nullable, test, afterEach } from 'vitest';
 
 import { render, fireEvent } from 'solid-testing-library';
 
+import { I18nContext, createI18nContext } from '@solid-primitives/i18n';
+
 import matchers from '@testing-library/jest-dom/matchers';
 
-import Comp from './Comp';
+import { default as Comp, cssClasses } from './Comp';
 
 expect.extend(matchers);
 
-describe('<Comp />', () => {
-    test('it will render a text', () => {
-        const { getByText, unmount } = render(() => <Comp text="test" />);
-        expect(getByText('test')).toBeInTheDocument();
-        unmount();
-    });
-
-    test('should have button', async () => {
-        const { getByText, unmount } = render(() => <Comp text="test" />);
-        expect(getByText('Flip')).toBeInTheDocument();
-        unmount();
-    });
-
-    test.each([
-        ['text', 'TEXT'],
-        ['TEXT', 'text'],
-    ])(
-        'should flip %p to %p on click back and forth',
-        async (text: string, expected: string) => {
-            const { getByText, unmount } = render(() => <Comp text={text} />);
-            const btn = getByText('Flip');
-            fireEvent.click(btn as HTMLInputElement);
-            expect(getByText(expected)).toBeInTheDocument();
-            fireEvent.click(btn as HTMLInputElement);
-            expect(getByText(text)).toBeInTheDocument();
-            unmount();
+function createLocaleDict() {
+    return {
+        ru: {
+            Comp: {
+                SwitchLang: 'Переключить язык',
+                Flip: 'Перевернуть',
+            },
         },
-    );
+        en: {
+            Comp: {
+                SwitchLang: 'Switch lang',
+                Flip: 'Flip',
+            },
+        },
+    };
+}
+
+const renderComp = (text?: string, lang?: string) => {
+    const dict = createLocaleDict();
+    const textVal = text || 'test';
+    const langVal = lang || 'en';
+    const value = createI18nContext(dict, langVal);
+    return render(() => (
+        <I18nContext.Provider value={value}>
+            <Comp text={textVal} />
+        </I18nContext.Provider>
+    ));
+};
+
+describe('<Comp />', () => {
+    let componentUnmount: Nullable<() => void> = null;
+
+    afterEach(() => {
+        if (componentUnmount) {
+            componentUnmount();
+        }
+    });
+
+    describe('Flip', () => {
+        test('it will render a text', () => {
+            const { getByText, unmount } = renderComp();
+            componentUnmount = unmount;
+            expect(getByText('test')).toBeInTheDocument();
+        });
+
+        test('should Flip have button', async () => {
+            const { container, unmount } = renderComp();
+            componentUnmount = unmount;
+            const btnFlip = container.getElementsByClassName(
+                cssClasses.btnFlip,
+            )[0];
+            expect(btnFlip).toBeInTheDocument();
+        });
+
+        test.each([
+            ['text', 'TEXT'],
+            ['TEXT', 'text'],
+        ])(
+            'should flip %s to %s on click back and forth',
+            async (text: string, expected: string) => {
+                const { container, getByText, unmount } = renderComp(text);
+                componentUnmount = unmount;
+                const btnFlip = container.getElementsByClassName(
+                    cssClasses.btnFlip,
+                );
+                fireEvent.click(btnFlip[0]);
+                expect(getByText(expected)).toBeInTheDocument();
+                fireEvent.click(btnFlip[0]);
+                expect(getByText(text)).toBeInTheDocument();
+            },
+        );
+    });
+
+    describe('Switch lang', () => {
+        const dict = createLocaleDict();
+
+        test('should have "switch lang" button', () => {
+            const { getByText, unmount } = renderComp();
+            componentUnmount = unmount;
+            expect(getByText(dict.en.Comp.SwitchLang)).toBeInTheDocument();
+        });
+
+        test.each([
+            [5, 'en', dict.en.Comp.SwitchLang],
+            [3, 'ru', dict.ru.Comp.SwitchLang],
+        ])(
+            '%d:[%s] "switch lang" button should switch lang and label on click from "%s" to "%s" and back.',
+            async (testNo: number, lang: string, expected: string) => {
+                const { unmount, container } = renderComp('', lang);
+                componentUnmount = unmount;
+
+                const getSwitchButton = (): [
+                    HTMLButtonElement,
+                    HTMLSpanElement,
+                ] => {
+                    const btn = container.getElementsByClassName(
+                        cssClasses.btnSwitchLang,
+                    )[0];
+                    expect(btn).toBeInTheDocument();
+                    const btnLabel = btn.getElementsByTagName('span')[0];
+                    expect(btnLabel).toBeInTheDocument();
+                    return [btn as HTMLButtonElement, btnLabel];
+                };
+
+                const [btn, btnLabel] = getSwitchButton();
+                expect(btnLabel.innerHTML).toBe(expected);
+                await fireEvent.click(btn);
+            },
+        );
+    });
 });
--- src/Comp.tsx
+++ src/Comp.tsx
@@ -1,4 +1,5 @@
 import { createSignal, Setter } from 'solid-js';
+import { useI18n } from '@solid-primitives/i18n';
 
 function flipCase(text: string, setText: Setter<string>) {
     if (text[0] == text[0].toLowerCase()) {
@@ -10,18 +11,52 @@ function flipCase(text: string, setText: Setter<string>) {
     }
 }
 
+function flipLang(currentLang: string): string {
+    let lang = 'en';
+    if (currentLang === 'en') {
+        lang = 'ru';
+    }
+    console.log(`flip ${currentLang} to ${lang}`);
+    return lang;
+}
+
+export const cssClasses = {
+    btnSwitchLang: 'tst-switch-lang',
+    btnFlip: 'tst-flip',
+};
+
 export default (props: { text: string }) => {
     const [text, setText] = createSignal<string>();
+    const [t, { locale }] = useI18n();
+
     const textGetter = () => text() || props.text;
+    const changeLang = () => {
+        const currentLang = locale();
+        const newLang = flipLang(currentLang);
+        locale(newLang);
+        console.log(
+            `Switch ${currentLang} to ${newLang}. Current lang ${locale()} ${t(
+                'Comp.SwitchLang',
+            )}`,
+        );
+    };
+
     return (
         <h1>
             <p>{textGetter()}</p>
             <button
-                class="rounded border-solid border-black border"
+                class={`rounded border-solid border-black border ${cssClasses.btnSwitchLang}`}
+                onClick={() => changeLang()}
+            >
+                <i class="i-mdi-account text-base"> </i>
+                <span>{t('Comp.SwitchLang')}</span>
+            </button>
+            <button
+                class={`rounded border-solid border-black border ${cssClasses.btnFlip}`}
                 onClick={() => flipCase(textGetter(), setText)}
             >
                 <i class="i-mdi-account text-base">T</i>
-                <span>Flip</span>
+                <span>{t('Comp.Flip')}</span>
             </button>
         </h1>
     );
